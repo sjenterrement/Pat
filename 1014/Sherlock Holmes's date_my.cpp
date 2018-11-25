0e3363c663f7bd2d58fdb5367cc917500e50f63e
sjenterrement <sjenterrement@gmail.com>
2018-11-25 22:21:55 +0800

    1014..first won

---
+++ 1014/Sherlock Holmes's date_my.cpp
@@ -0,0 +1,116 @@
+//grade:20
+#include<iostream>
+#include<vector>
+#include<string>
+#include<sstream>
+using namespace std;
+
+string get_Day(char c) {
+	string str;
+	switch (c)
+	{
+	case 'A':str = "MON"; break;
+	case 'B':str = "TUE"; break;
+	case 'C':str = "WED"; break;
+	case 'D':str = "THU"; break;
+	case 'E':str = "FRI"; break;
+	case 'F':str = "SAT"; break;
+	case 'G':str = "SUN"; break;
+	}
+	return str;
+}
+
+string get_Hour(char c) {
+	std::stringstream ss;
+	std::string str;
+	if (c >= '0'&&c <= '9')
+	{
+		int a = c - '0';
+		ss << a;
+		ss >> str;
+		return "0" + str;
+	}
+	else
+	{
+		int b = c - 'A' + 10;
+		ss << b;
+		ss >> str;
+		return str;
+	}
+}
+
+int main() {
+	string str1, str2, str3, str4;
+	int len1, len2, len3, len4, temp_len, loc;
+	cin >> str1 >> str2 >> str3 >> str4;
+	vector<string> v;
+
+	len1 = str1.length();
+	len2 = str2.length();
+	temp_len = len1 > len2 ? len2 : len1;
+
+	for (int i = 0; i < temp_len; i++)
+	{
+		if (str1[i] == str2[i])
+		{
+			if (str1[i] >= 'A'&&str1[i] <= 'G')
+			{
+				v.push_back(get_Day(str1[i]));
+				loc = i;
+				break;
+			}
+		}
+	}
+	v.push_back(" ");
+
+	for (int i = loc + 1; i < temp_len; i++)
+	{
+		if (str1[i] == str2[i])
+		{
+			if ((str1[i] >= '0'&&str1[i] <= '9') || (str1[i] >= 'A'&&str1[i] <= 'N'))
+			{
+				v.push_back(get_Hour(str1[i]));
+				break;
+			}
+		}
+	}
+
+	v.push_back(":");
+
+	len3 = str3.length();
+	len4 = str4.length();
+	temp_len = len3 > len4 ? len4 : len3;
+
+	for (int i = 0; i < temp_len; i++)
+	{
+		if (str3[i] == str4[i])
+		{
+			if ((str3[i] >= 'a'&&str3[i] <= 'z') || (str3[i] >= 'A'&&str3[i] <= 'Z'))
+			{
+				std::stringstream ss;
+				std::string str;
+				ss << i;
+				ss >> str;
+				if (i <= 9)
+				{
+
+					str = "0" + str;
+					v.push_back(str);
+				}
+				else
+				{
+					v.push_back(str);
+				}
+				break;
+			}
+
+		}
+	}
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i];
+	}
+
+	return 0;
+}
