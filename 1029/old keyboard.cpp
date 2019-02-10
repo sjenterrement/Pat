5870410a8db4a6ffc04557667d5ef07993c7a3e0
sjenterrement <sjenterrement@gmail.com>
2019-02-10 15:15:35 +0800

    1029

---
+++ 1029/old keyboard.cpp
@@ -0,0 +1,38 @@
+//grade:20!!
+#include<cstdio>
+#include<string>
+#include<iostream>
+using namespace std;
+
+int main() {
+	string s1, s2,s;
+	char ch;
+	cin >> s1 >> s2;
+	int l1 = s1.length();
+	string::size_type idx1,idx2;
+	for (int i = 0; i < l1; i++)
+	{
+		idx1 = s2.find(s1[i]);
+		if (idx1==string::npos)
+		{
+			if (isalpha(s1[i]))
+			{
+				ch = toupper(s1[i]);
+				idx2 = s.find(ch);
+				if (idx2==string::npos)
+				{
+					printf("%c", ch);
+				}
+				s += ch;
+				continue;
+			}
+			idx2 = s.find(s1[i]);
+			if (idx2 == string::npos)
+			{
+				cout << s1[i];
+			}
+			s += s1[i];
+		}
+	}
+	return 0;
+}
