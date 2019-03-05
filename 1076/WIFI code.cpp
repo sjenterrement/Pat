5d3e62a935527da6bf99390bd14f43a603996aa1
sjenterrement <sjenterrement@gmail.com>
2019-03-05 17:41:53 +0800

    1076

---
+++ 1076/WIFI code.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include<stdio.h>
+int main()
+{
+	char a, b, c[1001];
+	int n, i, k = 0;
+	scanf("%d%*c", &n);
+	while (n--)
+	{
+		for (i = 0; i < 4; i++)
+		{
+			scanf("%c-%c%*c", &a, &b);
+			if (b == 'T') c[k++] = a - 'A' + 1 + '0';
+		}
+	}
+	c[k] = '\0';
+	puts(c);
+	return 0;
+}
