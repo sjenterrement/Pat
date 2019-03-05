9110adefa661b7fbfec7bdc143d82174b38a0f78
sjenterrement <sjenterrement@gmail.com>
2019-03-05 22:52:31 +0800

    1080...未完成

---
+++ 1080/mooc.cpp
@@ -0,0 +1,22 @@
+#include<cstdio>
+#include<string>
+#include<vector>
+using namespace std;
+
+struct student
+{
+	string id;
+	float ur;
+	float mt;
+	float fn;
+}stdt;
+
+vector<student> v;
+int P, M, N;
+
+int main() {
+	scanf("%d%d%d", &P, &M, &N);
+
+
+	return 0;
+}
