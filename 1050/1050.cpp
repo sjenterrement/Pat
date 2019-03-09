8f7608690e0c957de9aa35e731ee40bf59a94896
sjenterrement <sjenterrement@gmail.com>
2019-03-09 15:31:35 +0800

    1050

---
+++ 1050/1050.cpp
@@ -0,0 +1,46 @@
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+
+int cmp(int a,int b) {
+	return a > b;
+}
+
+int main() {
+	int N, n, m, t = 0;
+	scanf_s("%d", &N);
+	for (n = sqrt(N); n>=1; n--) {
+		if (N%n == 0) {
+			m = N / n;
+			break;
+		}
+	}
+	vector<int> a(N);
+	for (int i = 0; i < N; i++)
+		scanf_s("%d", &a[i]);
+	sort(a.begin(), a.end(), cmp);
+	vector<vector<int>> b(m, vector<int>(n));
+	int level = m / 2 + m % 2;
+	for (int i = 0; i < level; i++) {
+		for (int j = i; j <= n - 1 - i && t <= N - 1; j++)
+			b[i][j] = a[t++];
+		for (int j = i + 1; j <= m - 2 - i && t <= N - 1; j++)
+			b[j][n - 1 - i] = a[t++];
+		for (int j = n - 1 - i; j >= i && t <= N - 1; j--)
+			b[m - 1 - i][j] = a[t++];
+		for (int j = m - 2 - i; j >= i + 1 && t <= N - 1; j--)
+			b[j][i] = a[t++];
+	}
+
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%d", b[i][j]);
+			if (j != n - 1) printf(" ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
