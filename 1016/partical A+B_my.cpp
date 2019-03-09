35275778331ced9c9ef5e673015abfd346036f16
sjenterrement <sjenterrement@gmail.com>
2019-03-09 11:20:48 +0800

    1016 review

--- 1016/partical A+B_my.cpp
+++ 1016/partical A+B_my.cpp
@@ -1,4 +1,4 @@
-//grade:15
+/*//grade:15
 #include<iostream>
 using namespace std;
 
@@ -8,7 +8,7 @@ int main() {
 	int P_A = 0, P_B = 0;
 	int temp;
 	cin >> A >> D_A >> B >> D_B;
-	
+
 	temp = A;
 	while (temp)
 	{
@@ -29,13 +29,55 @@ int main() {
 	{
 		P_A = P_A * 10 + D_A;
 	}
-	
+
 	for (int i = 0; i < count_B; i++)
 	{
 		P_B = P_B * 10 + D_B;
 	}
 
 	cout << P_A + P_B;
+
+	return 0;
+}*/
+
+#include<string>
+#include<iostream>
+using namespace std;
+int main() {
+	string a, b, spa = "", spb = "";
+	int da, db, pa, pb;
+	cin >> a >> da >> b >> db;
+	for (int i = 0; i < a.length(); i++) {
+		if (da == (a[i] - '0'))
+		{
+			spa += a[i];
+		}
+	}
+	for (int i = 0; i < b.length(); i++) {
+		if (db == (b[i] - '0'))
+		{
+			spb += b[i];
+		}
+	}
+
+	if (spa.empty() && !spb.empty()) {
+		pa = 0;
+		pb = stoi(spb);
+	}
+	else if (!spa.empty() && spb.empty()) {
+		pa = stoi(spa);
+		pb = 0;
+	}
+	else if (spa.empty() && spb.empty()) {
+		pa = 0;
+		pb = 0;
+	}
+	else
+	{
+		pa = stoi(spa);
+		pb = stoi(spb);
+	}
+	cout << pa + pb;
 
 	return 0;
 }
