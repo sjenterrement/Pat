933aa6713320d880967d37b610bf60ee8a84a70a
sjenterrement <sjenterrement@gmail.com>
2019-03-12 20:22:25 +0800

    1067

--- 1067/password.cpp
+++ 1067/password.cpp
@@ -1,4 +1,4 @@
-//14/20
+/*//14/20
 #include<cstdio>
 #include<vector>
 #include<string>
@@ -49,6 +49,33 @@ int main() {
 	else if(count>=n)
 	{
 		cout << "Account locked";
+	}
+
+	return 0;
+}*/
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+	string password,temp;
+	int n,cnt = 0;
+	cin >> password >> n;
+	getchar();
+	while (1) {
+		getline(cin, temp);
+		if (temp == "#") break;
+		cnt++;
+		if (cnt <= n && temp == password) {
+			cout << "Welcome in";
+			break;
+		}
+		else if(cnt<=n&&temp!=password){
+			cout << "Wrong password: " << temp << endl;
+			if (cnt == n) {
+				cout << "Account locked";
+				break;
+			}	
+		}
 	}
 
 	return 0;
