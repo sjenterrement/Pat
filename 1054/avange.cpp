750c55ca7a3f3a61a75b0d644c1994b962418c92
sjenterrement <sjenterrement@gmail.com>
2019-03-12 17:28:54 +0800

    1054

--- 1054/avange.cpp
+++ 1054/avange.cpp
@@ -1,4 +1,4 @@
-//17/20
+/*
 #include<cstdio>
 #include<string>
 #include<iostream>
@@ -57,5 +57,38 @@ int main() {
 		printf("The average of %d numbers is %.2f", count, ans / count);
 	}
 
+	return 0;
+}*/
+#include<iostream>
+#include<string>
+#include<cstdio>
+using namespace std;
+int main() {
+	int n = 0, cnt = 0;
+	char a[50], b[50];
+	double temp, sum = 0.0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		sscanf_s(a, "%lf", &temp);
+		sprintf_s(b, "%.2f", temp);
+		int flag = 0;
+		for (int j = 0; j < strlen(a); j++)
+			if (a[j] != b[j]) flag = 1;
+		if (flag || temp < -1000 || temp>1000) {
+			printf("ERROR: %s is not a legal number\n", a);
+			continue;
+		}
+		else {
+			sum += temp;
+			cnt++;
+		}
+	}
+	if (cnt == 1)
+		printf("The average of 1 number is %.2f", sum);
+	else if (cnt > 1)
+		printf("The average of %d numbers is %.2f", cnt, sum / cnt);
+	else
+		printf("The average of 0 number is Undefined");
 	return 0;
 }
