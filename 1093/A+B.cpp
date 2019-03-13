d7e0615c4d2d560e9992659545a8324bf6fbddd7
sjenterrement <sjenterrement@gmail.com>
2019-03-13 10:12:09 +0800

    1093 review

--- 1093/A+B.cpp
+++ 1093/A+B.cpp
@@ -1,5 +1,5 @@
 //20/20
-#include<unordered_set>
+/*#include<unordered_set>
 #include<string>;
 #include<iostream>
 using namespace std;
@@ -31,5 +31,21 @@ int main() {
 
 	cout << s;
 
+	return 0;
+}*/
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+	string s1, s2, s;
+	int hash[200] = { 0 };
+	getline(cin, s1);
+	getline(cin, s2);
+	s = s1 + s2;
+	for (int i = 0; i < s.length(); i++) {
+		if (hash[s[i]] == 0)
+			cout << s[i];
+		hash[s[i]] = 1;
+	}
 	return 0;
 }
