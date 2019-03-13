d77dad2e7bceae84547a858b14cf3600b06ef73e
sjenterrement <sjenterrement@gmail.com>
2019-03-13 10:20:43 +0800

    1094 review

--- 1094/google test.cpp
+++ 1094/google test.cpp
@@ -1,4 +1,4 @@
-//18/20
+/*//18/20
 #include<string>
 #include<iostream>
 #include<cmath>
@@ -41,7 +41,47 @@ int main() {
 		}
 	}
 
-	cout << "404";
+	cout << "404\n";
+
+	return 0;
+}*/
+#include<string>
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+bool is_prime(int x) {
+	if (x == 0 || x == 1)
+	{
+		return false;
+	}
+	for (int i = 2; i*i <= x; i++)
+	{
+		if (x%i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int L, K;
+string s;
+
+int main() {
+	cin >> L >> K;
+	cin >> s;
+	for (int i = 0; i <= L - K; i++)
+	{
+		string temps = s.substr(i, K);
+		int tempi = stoi(temps);
+		if (is_prime(tempi)) {
+			cout << temps;
+			return 0;
+		}
+	}
+
+	cout << "404\n";
 
 	return 0;
 }
