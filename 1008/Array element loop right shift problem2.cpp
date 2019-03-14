08689a8eadd934ba66c76bbea604081f487c74a5
sjenterrement <sjenterrement@gmail.com>
2019-03-14 14:58:56 +0800

    1008 review

--- 1008/Array element loop right shift problem2.cpp
+++ 1008/Array element loop right shift problem2.cpp
@@ -1,10 +1,11 @@
+/*
 #include<iostream>
 #include<list>
 using namespace std;
 /*
 *ÀûÓÃlist
 */
-int main() {
+/*int main() {
 	int n, shift, temp;
 	cin >> n >> shift;
 	list<int> mylist;
@@ -36,3 +37,28 @@ int main() {
 	cout << endl;
 	return 0;
 }
+*/
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int mina() {
+	int n, m;
+	cin >> n >> m;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	m %= n;
+	if (m != 0) {
+		reverse(begin(a), begin(a) + n);
+		reverse(begin(a), begin(a) + m);
+		reverse(begin(a), begin(a) + n);
+	}
+	for (int i = 0; i < n - 1; i++) {
+		cout << a[i] << " ";
+	}
+	cout << a[n - 1];
+	return 0;
+}
+
