00a2518178325c4d02107513939bbb82066aaaff
sjenterrement <sjenterrement@gmail.com>
2019-03-14 16:37:19 +0800

    1022

--- 1022/D-ary A B.cpp
+++ 1022/D-ary A B.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 #include<vector>
 using namespace std;
 
@@ -25,4 +25,25 @@ int main() {
 	}
 	return 0;
 }
-
+*/
+#include<iostream>
+using namespace std;
+int main() {
+	int a, b, d;
+	cin >> a >> b >> d;
+	int t = a + b;
+	if (t == 0) {
+		cout << 0;
+		return 0;
+	}
+	int s[100];
+	int i = 0;
+	while (t != 0) {
+		s[i++] = t % d;
+		t = t / d;
+	}
+	for (int j = i - 1; i >= 0; j--) {
+		cout << s[j];
+	}
+	return 0;
+}
