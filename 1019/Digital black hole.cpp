a82ed8d1481c7d2c819e657a300483853431e5a1
sjenterrement <sjenterrement@gmail.com>
2019-03-14 15:45:03 +0800

    1019 review

--- 1019/Digital black hole.cpp
+++ 1019/Digital black hole.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 #include<vector>
 #include<algorithm>
 #include<iomanip>
@@ -31,5 +31,28 @@ int main()
 	}
 
 	system("pause");
+	return 0;
+}
+*/
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+bool cmp(char a, char b) {
+	return a > b;
+}
+int main() {
+	string s;
+	cin >> s;
+	s.insert(0, 4 - s.length(), '0');
+	do {
+		string a = s, b = s;
+		sort(a.begin(), a.end(), cmp);
+		sort(b.begin(), b.end());
+		int result = stoi(a) - stoi(b);
+		s = to_string(result);
+		s.insert(0, 4 - s.length(), '0');
+		cout << a << "-" << b << "=" << s << endl;
+	} while (s != "6174"&&s != "0000");
 	return 0;
 }
