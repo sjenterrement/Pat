d18a23151c45d379362e8c1718af6c81066868b6
sjenterrement <sjenterrement@gmail.com>
2019-03-14 16:26:57 +0800

    1020 review

--- 1020/moon cake.cpp
+++ 1020/moon cake.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 #include<vector>
 #include<algorithm>
 #include<iomanip>
@@ -53,6 +53,39 @@ int main() {
 	}
 
 	cout << setiosflags(ios::fixed) << setprecision(2) << value << endl;
-	
+
+	return 0;
+}
+*/
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+struct mooncake{
+	float mount, price, unit;
+};
+int cmp(mooncake a,mooncake b) {
+	return a.unit > b.unit;
+}
+int main() {
+	int n, need;
+	cin >> n >> need;
+	vector<mooncake> a(n);
+	for (int i = 0; i < n; i++) cin >> a[i].mount;
+	for (int i = 0; i < n; i++) cin >> a[i].price;
+	for (int i = 0; i < n; i++) a[i].unit = a[i].price / a[i].mount;
+	sort(a.begin(), a.end(), cmp);
+	float result = 0.0;
+	for (int i = 0; i < n; i++) {
+		if (a[i].mount <= need) {
+			result = result + a[i].price;
+		}
+		else {
+			result = result + a[i].unit*need;
+			break;
+		}
+		need -= a[i].mount;
+	}
+	printf("%.2f", result);
 	return 0;
 }
