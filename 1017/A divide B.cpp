e434e753bf7e067d3bde9739d1449497e8f1c568
sjenterrement <sjenterrement@gmail.com>
2019-03-14 15:24:24 +0800

    1017 review

--- 1017/A divide B.cpp
+++ 1017/A divide B.cpp
@@ -1,3 +1,4 @@
+/*
 #include<iostream>
 #include<string>
 using namespace std;
@@ -26,4 +27,24 @@ int main() {
 	{
 		cout << Q <<" "<< R;
 	}
+}*/
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+	string s;
+	int a, t = 0, temp = 0;
+	cin >> s >> a;
+	int len = s.length();
+	t = (s[0] - '0') / a;
+	if ((t != 0 && len > 1) || len == 1)
+		cout << t;
+	temp = (s[0] - '0') % a;
+	for (int i = 1; i < len; i++) {
+		t = (temp * 10 + s[i] - '0') / a;
+		cout << t;
+		temp = (temp * 10 + s[i] - '0') % a;
+	}
+	cout << " " << temp;
+	return 0;
 }
