081ba8ef772c4bf4f93a013337b77b0381068200
sjenterrement <sjenterrement@gmail.com>
2019-03-14 16:59:23 +0800

    1023 review

--- 1023/smallest number.cpp
+++ 1023/smallest number.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 #include<string>
 #include<vector>
 using namespace std;
@@ -26,5 +26,30 @@ int main()
 	}
 	cout << endl;
 	system("pause");
+	return 0;
+}
+*/
+#include<iostream>
+using namespace std;
+int main() {
+	int a[10], t;
+	for (int i = 0; i < 10; i++) {
+		cin >> a[i];
+	}
+	for (int i = 1; i < 10; i++) {
+		if (a[i] != 0) {
+			cout << i;
+			t = i;
+			break;
+		}
+	}
+	for (int i = 0; i < a[0]; i++) cout << 0;
+	for (int i = 0; i < a[t] - 1; i++) cout << t;
+	for (int i = t+1; i < 10; i++) {
+		for (int j = 0; i < a[i]; j++) {
+			cout << i;
+		}
+	}
+
 	return 0;
 }
