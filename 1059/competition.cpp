018bc7245b0d2386632d35ccfe219c327d7acc9e
sjenterrement <sjenterrement@gmail.com>
2019-03-15 14:22:41 +0800

    1059 review

--- 1059/competition.cpp
+++ 1059/competition.cpp
@@ -1,4 +1,4 @@
-//14/20
+/*//14/20
 #include<cstdio>
 #include<vector>
 #include<string>
@@ -90,5 +90,57 @@ int main() {
 		}
 	}
 
+	return 0;
+}*/
+
+#include<iostream>
+#include<set>
+#include<cmath>
+using namespace std;
+int ran[10000];
+bool isprime(int a) {
+	if (a <= 1) return false;
+	int Sqrt = sqrt((double)a);
+	for (int i = 2; i <= Sqrt; i++) {
+		if (a%i == 0)
+			return false;
+	}
+	return true;
+}
+int main() {
+	int n, k;
+	scanf_s("%d", &n);
+	for (int i = 0; i < n; i++) {
+		int id;
+		scanf_s("%d", &id);
+		ran[id] = i + 1;
+	}
+	scanf_s("%d", &k);
+	set<int> ss;
+	for (int i = 0; i < k; i++) {
+		int id;
+		scanf_s("%d", &id);
+		printf("%04: ", id);
+		if (ran[id] == 0) {
+			printf("Are you kidding?\n");
+			continue;
+		}
+		if (ss.find(id) == ss.end()) {
+			ss.insert(id);
+		}
+		else {
+			printf("Checked\n");
+			continue;
+		}
+		if (ran[id] == 1) {
+			printf("Mystery Award\n");
+		}
+		else if (isprime(ran[id])) {
+			printf("Minion\n");
+		}
+		else {
+			printf("Chocolate\n");
+		}
+	}
 	return 0;
 }
