3ed08572bbec732db64b75f9f3aeecffb0afdcf0
sjenterrement <sjenterrement@gmail.com>
2019-03-15 11:54:30 +0800

    1040 review

--- 1040/num PAT.cpp
+++ 1040/num PAT.cpp
@@ -1,4 +1,4 @@
-#include<cstdio>
+/*
 #include<iostream>
 #include<string>
 using namespace std;
@@ -20,5 +20,26 @@ int main()
 	}
 
 	printf("%d", num);
+	return 0;
+}
+*/
+
+#include<string>
+#include<iostream>
+using namespace std;
+int main() {
+	string s;
+	cin >> s;
+	int len = s.length(), result = 0, countp = 0, countt = 0;
+	for (int i = 0; i < len; i++) {
+		if (s[i] == 'T')
+			countt++;
+	}
+	for (int i = 0; i < len; i++) {
+		if (s[i] == 'P') countp++;
+		if (s[i] == 'T') countt--;
+		if (s[i] == 'A') result = (result + (countp*countt) % 1000000007) % 1000000007;
+	}
+	cout << result;
 	return 0;
 }
