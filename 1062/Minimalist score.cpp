c6e8993c9528d9d076d0e236eefab5443aed7620
sjenterrement <sjenterrement@gmail.com>
2019-03-15 15:37:10 +0800

    1062 review

--- 1062/Minimalist score.cpp
+++ 1062/Minimalist score.cpp
@@ -1,4 +1,4 @@
-#include<cstdio>
+/*#include<cstdio>
 using namespace std;
 double a, b, c, d, e;
 double a1, a2, a3;
@@ -36,5 +36,28 @@ int main() {
 	}
 	printf("\n");
 
+	return 0;
+}*/
+#include<iostream>
+using namespace std;
+int gcd(int a, int b) {
+	return b == 0 ? a : gcd(b, a%b);
+}
+int main() {
+	int n1, m1, n2, m2, k;
+	scanf_s("%d/%d %d/%d %d", &n1, &m1, &n2, &m2, &k);
+	if (n1*m2 > n2*m1) {
+		swap(n1, n2);
+		swap(m1, m2);
+	}
+	int num = 1;
+	bool flag = false;
+	while (n1*k >= m1 * num) num++;
+	while (n1*k < m1*num&&m2*num < n2*k) {
+		if (gcd(num, k) == 1) {
+			printf("%s%d/%d", flag == true ? " " : "", num, k);
+		}
+		num++;
+	}
 	return 0;
 }
