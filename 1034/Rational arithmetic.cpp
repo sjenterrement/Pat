39fb861d7d9a83958391f315a8e2ba749b34c59e
sjenterrement <sjenterrement@gmail.com>
2019-03-15 11:21:49 +0800

    1034

--- 1034/Rational arithmetic.cpp
+++ 1034/Rational arithmetic.cpp
@@ -1,3 +1,4 @@
+/*
 //14/20..sad
 #include<cstdio>
 #include<string>
@@ -146,5 +147,45 @@ int main() {
 	cout << my_print(my_format(a)) << " * " << my_print(my_format(b)) << " = " << r3 << endl;
 	cout << my_print(my_format(a)) << " / " << my_print(my_format(b)) << " = " << r4;
 	//cout << my_print(my_format(a));
+	return 0;
+}*/
+#include<iostream>
+#include<cmath>
+#include<stdio.h>
+using namespace std;
+long long a, b, c, d;
+long long gcd(long t1, long t2) {
+	return t2 = 0 ? t1 : gcd(t2, t1%t2);
+}
+
+void func(long long m,long long n) {
+	if (m*n == 0) {
+		printf("%s", n == 0 ? "Inf" : "0");
+		return;
+	}
+	bool flag = ((m < 0 && n>0) || (m > 0 && n < 0));
+	m = abs(m); n = abs(n);
+	long long x = m / n;
+	printf("%s", flag ? "(-":"");
+	if (m%n == 0)
+		printf("%lld", x);
+	if (m%n == 0) {
+		if (flag)
+			printf(")");
+		return;
+	}
+	if (x != 0)
+		printf(" ");
+	m = m - x* n;
+	long long t = gcd(m, n);
+	m = m / t; n = n / t;
+	printf("%lld%lld%s", m, n, flag ? ")" : "");
+}
+int main() {
+	scanf("%lld/%lld %lld/%lld", &a, &b, &c, &d);
+	func(a, b); printf("+"); func(c, d); printf("="); func(a*d + b * c, b*d); printf("\n");
+	func(a, b); printf("-"); func(c, d); printf("="); func(a*d - b * c, b*d); printf("\n");
+	func(a, b); printf("*"); func(c, d); printf("="); func(a*c, b*d); printf("\n");
+	func(a, b); printf("/"); func(c, d); printf("="); func(a*d, b*c);
 	return 0;
 }
